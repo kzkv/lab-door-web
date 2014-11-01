288b851d63f72b76ba59bf8d334be8d1a5acd4e8
Tyoma Kazakov <tyoma@kzkv.ru>
2014-11-02 01:10:56 +0300

    lab door web service initial commit

---
+++ lab-door.py
@@ -0,0 +1,29 @@
+# -*- coding: UTF-8 -*-
+
+import RPi.GPIO as GPIO
+from time import sleep
+
+from flask import Flask
+app = Flask(__name__)
+    # веб-сервис Фласка
+
+GPIO.setmode(GPIO.BCM)             
+    # нумерация портов через BCM  
+
+GPIO.setup(24, GPIO.OUT)
+    # использовать пин 24 для вывода    
+
+
+@app.route("/open")
+def openDoor():
+    GPIO.output(24, True)
+    sleep(1)
+    GPIO.output(24, False)
+        
+    message = "GPIO24 ON"
+    templateData = {
+    }
+    return message
+
+
+
