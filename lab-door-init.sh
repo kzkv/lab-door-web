1de22421adde930352e61408efab89cabf889844
Tyoma Kazakov <tyoma@kzkv.ru>
2014-11-02 15:39:43 +0300

    errors output

--- lab-door-init.sh
+++ lab-door-init.sh
@@ -2,7 +2,7 @@
 
 case "$1" in
   start)
-    nohup python /home/pi/Git/lab-door-web/lab-door.py &
+    nohup python /home/pi/Git/lab-door-web/lab-door.py > /nome/pi/Documents/lab-door.out 2> /nome/pi/Documents/lab-door.err < /dev/null &
     ;;
   stop)
     killall python
