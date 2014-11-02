86ea1b3f34ecc90316723f45a803da432b7a908b
Tyoma Kazakov <tyoma@kzkv.ru>
2014-11-02 15:40:27 +0300

    typos

--- lab-door-init.sh
+++ lab-door-init.sh
@@ -2,7 +2,7 @@
 
 case "$1" in
   start)
-    nohup python /home/pi/Git/lab-door-web/lab-door.py > /nome/pi/Documents/lab-door.out 2> /nome/pi/Documents/lab-door.err < /dev/null &
+    nohup python /home/pi/Git/lab-door-web/lab-door.py > /home/pi/Documents/lab-door.out 2> /home/pi/Documents/lab-door.err < /dev/null &
     ;;
   stop)
     killall python
